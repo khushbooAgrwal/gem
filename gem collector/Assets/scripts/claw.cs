bc29b297079a6f77393709dff391dcbdc4af6dce
studymattersalot <63383520+studymattersalot@users.noreply.github.com>
2020-06-13 14:53:24 +0530

    unity

---
+++ gem collector/Assets/scripts/claw.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class claw : MonoBehaviour
+{
+   public Transform Origin = null;
+   public float speed = 4f;
+   public turret turretScript = null ;
+   public int GemValue = 100;
+   private Vector3 target;
+   private GameObject childobject =null;
+   private LineRenderer lineRenderer = null;
+   private bool Isgemhit = false;
+   private bool Isretracting = false;
+   void Awake() {
+       lineRenderer = GetComponent<LineRenderer>();
+   }
+    void Update() {
+       float step = speed * Time.deltaTime;
+       transform.position = Vector3.MoveTowards(transform.position,target,step);
+       lineRenderer.SetPosition(0,Origin.position);
+       lineRenderer.SetPosition(1,Origin.position);
+
+       if(transform.position == Origin.position && Isretracting && turretScript != null){
+           turretScript.CollectedObject();
+           if(Isgemhit){
+               Isgemhit = false;
+           }
+           if(childobject != null){
+               Destroy(childobject);
+           }
+           gameObject.SetActive(false);
+           Isretracting = false;
+       }
+   }
+   public void clawtarget(Vector3 targetpos){
+       target = targetpos;
+   }
+   void OnTriggerEnter(Collider other) {
+       if(Isretracting){
+           Isretracting = true;
+           target = Origin.position;
+           if(other.gameObject.CompareTag("Gem"))
+           Isgemhit = true;
+           if(!other.gameObject.CompareTag("Barrier")){
+               childobject = other.gameObject;
+               other.transform.SetParent(this.transform);
+           }
+       }
+   }
+}
---
+++ gem collector/Assets/scripts/turret.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turret : MonoBehaviour
+{
+   public GameObject claw = null;
+   public Animator AnimController_gun = null;
+   private bool IsShooting = false;
+   private LayerMask currentmask;
+   public bool Isshooting
+   {
+       get
+       {
+           return IsShooting;
+       }
+   }
+   void Update() {
+       if(Input.GetButtonDown("Fire1"))
+       {
+           Launchclaw();
+       }
+   }
+   private void Launchclaw(){
+       IsShooting = true;
+       if(AnimController_gun != null)
+       AnimController_gun.speed = 0f;
+
+       RaycastHit hit;
+       Vector3 down = transform.TransformDirection(Vector3.down);
+       if(Physics.Raycast(transform.position,down,out hit,100f))
+       {
+            if(claw != null ){
+                claw.SetActive(true);
+                claw.GetComponent<claw>().clawtarget(hit.point);
+            }
+       }
+   }
+   public void CollectedObject(){
+       IsShooting = false;
+
+       if(AnimController_gun != null){
+           AnimController_gun.speed = 1f;
+       }
+   }
+}
